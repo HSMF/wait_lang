233530877a33c90b82798615ce2991843114e0a5
Conradin Laux <conradin.laux@gmail.com>
2026-09-12 01:05:58 +0200

    basic definition of an IR

--- theories/Basic.v
+++ theories/Basic.v
@@ -49,13 +49,80 @@ Inductive item : Set :=
   Func : nat → typ → list statement → item
 .
 
+Module Ir.
+  Inductive label : Set :=
+    Label : nat → label
+  .
+
+  Inductive ident : Set :=
+    Local : nat → ident
+  .
+
+  Inductive binop_kind : Set :=
+    | BAdd : binop_kind
+    | BSub : binop_kind
+    | BMul : binop_kind
+    | BDiv : binop_kind
+    | BEq : binop_kind
+    | BLess : binop_kind
+    | BLessEq : binop_kind
+    | BGreater : binop_kind
+    | BGreaterEq : binop_kind
+  .
+
+  Inductive unop_kind : Set :=
+    Neg : unop_kind
+  .
+
+  Inductive typ : Set :=
+    | Int64 : typ
+    | Uint64 : typ
+    | Ptr : typ → typ
+  .
+
+  Inductive operand : Set :=
+    | Ident : ident → operand
+    | Int : nat → operand
+  .
+
+  Inductive term : Set :=
+    | Jump : label → term
+    | Return : operand → term
+    | CondJump : operand → label → label → term
+  .
+
+  Inductive operation : Set :=
+    | Binop : operand → binop_kind → operand → operation
+    | Unop : unop_kind → operand → operation
+    | Alloca : typ → operation
+    | Load : operand → operation
+    | Store : ident → operand → operation
+  .
+
+  Inductive instr : Set :=
+    (* dst, operation *)
+    | Instr : ident → operation → instr
+  .
+
+  Inductive basic_block : Set :=
+    BasicBlock : label → list instr → term → basic_block
+  .
+
+  Inductive func : Set :=
+    Func : nat → basic_block → list basic_block → func
+  .
+End Ir.
+
 Definition compile ( e : item ) : nat :=
   match e with
   | Func name ret body => name
   end.
 
+Definition foo ( e : Ir.func ) : nat :=
+  0
+.
 
 Require Extraction.
 Extraction Language OCaml.
-Extraction "extracted.ml" plus compile.
+Extraction "extracted.ml" plus compile foo.
 
